c00d852917415083154695ab939ec2f464994bc1
VanessaUfla <138214737+VanessaUfla@users.noreply.github.com>
2023-07-21 08:32:15 -0300

    Add files via upload
    espaçamento

---
+++ canva.py
@@ -0,0 +1,17 @@
+def adjust_letter_spacing(text, spacing):
+    adjusted_text = " "
+    for char in text:
+        adjusted_text += char + " " * spacing
+    return adjusted_text
+
+# Exemplo de uso:
+texto_original = "Bem vindo ao canva."
+espacamento_desejado = 2
+texto_ajustado = adjust_letter_spacing(texto_original, espacamento_desejado)
+
+print("Texto original:")
+print(texto_original)
+
+print("\nTexto com espaçamento ajustado:")
+print(texto_ajustado)
+
